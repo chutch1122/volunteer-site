6f8b8e2ca208a887d7100d6996d1b08e9351f310
Cameron Hutchison <chutch112292@gmail.com>
2016-07-23 14:43:32 -0500

    Refinement to registration page, fixed errors view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,76 +4,43 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
-                        {{ csrf_field() }}
+            <div class="card">
+                <div class="card-block">
+                    <div class="card-title">
+                        <h4>Register</h4>
+                    </div>
+                    <div class="card-text">
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+                        @include('shared.errors')
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}">
+                        <form method="POST" action="{{ url('/register') }}">
+                            {{ csrf_field() }}
 
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
+                            <div class="md-form">
+                                {!! Form::text('name', null, ['class' => 'form-control']) !!}
+                                {!! Form::label('name', 'Full Name') !!}
                             </div>
-                        </div>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+                            <div class="md-form">
+                                {!! Form::email('email', null, ['class' => 'form-control']) !!}
+                                {!! Form::label('email', 'Email Address') !!}
                             </div>
-                        </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password">
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
+                            <div class="md-form">
+                                {!! Form::password('password', null, ['class' => 'form-control']) !!}
+                                {!! Form::label('password', 'Password') !!}
                             </div>
-                        </div>
 
-                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation">
-
-                                @if ($errors->has('password_confirmation'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                    </span>
-                                @endif
+                            <div class="md-form">
+                                {!! Form::password('password_confirmation', null, ['class' => 'form-control']) !!}
+                                {!! Form::label('password_confirmation', 'Confirm Password') !!}
                             </div>
-                        </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-user"></i> Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                            <button type="submit" class="btn btn-primary">
+                                <i class="fa fa-btn fa-user"></i> Register
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/shared/errors.blade.php
+++ resources/views/shared/errors.blade.php
@@ -1,19 +1,10 @@
-@if(isset($errors))
-    @if(is_array($errors))
-        <div class="alert alert-danger" role="alert">
-            <strong>Uh oh!</strong> Change a few things up and try submitting again.
-            <ul>
-                @foreach ($errors as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @else
-        <div class="alert alert-danger" role="alert">
-            <strong>Uh oh!</strong> Looks like there Change a few things up and try submitting again.
-            <ul>
-                <li>{{ $errors }}</li>
-            </ul>
-        </div>
-    @endif
+@if(isset($errors) && count($errors) > 0)
+    <div class="alert alert-danger" role="alert">
+        <strong>Uh oh!</strong> Change a few things up and try submitting again.
+        <ul>
+            @foreach ($errors as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
 @endif
