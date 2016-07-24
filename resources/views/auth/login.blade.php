1c0afc9e531df806560dc0bae59f98dc568daef6
Cameron Hutchison <chutch112292@gmail.com>
2016-07-24 13:32:27 -0500

    Fixed login page styling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,60 +4,37 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                        {{ csrf_field() }}
+            <div class="card">
+                <div class="card-block">
+                    <div class="card-title">
+                        <h4>Login</h4>
+                    </div>
+                    <div class="card-text">
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                        @include('shared.errors')
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
+                        <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+                            {{ csrf_field() }}
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+                            <div class="md-form">
+                                {!! Form::email('email', old('email'), ['class' => 'form-control', 'placeholder' => ' ']) !!}
+                                {!! Form::label('email', 'Email Address') !!}
                             </div>
-                        </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password">
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
+                            <div class="md-form">
+                                {!! Form::password('password', null, ['class' => 'form-control', 'placeholder' => ' ']) !!}
+                                {!! Form::label('password', 'Password') !!}
                             </div>
-                        </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
+                            <label>
+                                <input type="checkbox" name="remember"> Remember Me
+                            </label>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-sign-in"></i> Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
-                            </div>
-                        </div>
-                    </form>
+                            <button type="submit" class="btn btn-primary btn-block">
+                                <i class="fa fa-btn fa-user"></i> Login
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
