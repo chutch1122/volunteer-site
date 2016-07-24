791a6e0306f9614a1d5de6d2cc90d23dfc2256ea
Geile, Dylan <decegeile@gmail.com>
2016-07-24 03:02:07 -0500

    Fix type in org pivot tables

--- database/migrations/2016_07_24_074523_create_organization_pivot_tables.php
+++ database/migrations/2016_07_24_074523_create_organization_pivot_tables.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class OrganizationPivotTables extends Migration
+class CreateOrganizationPivotTables extends Migration
 {
     /**
      * Run the migrations.
