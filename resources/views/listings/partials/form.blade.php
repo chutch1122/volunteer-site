3c90b447b7555506caa06a844e8bdf441c977715
ChrisS Admin <csadmin@utilities.local>
2016-07-24 12:09:41 -0500

    Added dates for start date on listing

--- resources/views/listings/partials/form.blade.php
+++ resources/views/listings/partials/form.blade.php
@@ -7,6 +7,7 @@
 </div>
 <div class="md-form">
     {!! Form::date('starts_at', \Carbon\Carbon::now()) !!}
+    {!! Form::label('starts_at', 'Beginning date') !!}
 </div>
 <div class="md-form">
     {!! Form::textarea('description', null, ['class' => 'md-textarea']) !!}
